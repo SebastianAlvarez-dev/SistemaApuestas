26ec3a987e505d1340caa24f9f81cea025ff1ff5
Sebastian Alvarez <sebastianear2005@gmail.com>
2026-06-02 14:42:45 -0500

    añadiendo sonar qube para .NET

--- Aplicacion/obj/Debug/net10.0/Aplicacion.AssemblyInfo.cs
+++ Aplicacion/obj/Debug/net10.0/Aplicacion.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aplicacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dda50d591720dadf231bdd8b010425a2b835e362")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4987445d6ef4c79d0ac141d1a2b75010d09aa23f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aplicacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aplicacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
